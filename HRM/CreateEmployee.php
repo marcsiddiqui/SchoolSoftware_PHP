7475c76a0421c4d687d6112def67e1157ff48044
Muhammad Arsalan <marcsiddiqui@gmail.com>
2023-06-02 10:56:58 +0500

    new files

---
+++ HRM/CreateEmployee.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container mt-3">
+  <a href="http://localhost:82/sms/hrm/Employee.php" type="button" class="btn btn-secondary">Secondary</a>
+</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,49 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>SMS</title>
-    </head>
+<head>
+	<style>
+		img {
+			width: 500px;
+			height: 500px;
+			border: 1px red solid;
+		}
+		h1 {
+			color: Green;
+		}
+		body {
+			background: white;
+			margin-top: 4%;
+			margin-left: 10%;
+		}
+	</style>
+</head>
+<body>
+	<h1>GeeksforGeeks</h1>
+	<h2>Creating 4 equal clickable areas in an image</h2>
+	<img src="https://www.imgonline.com.ua/examples/snow.jpg" alt="usemap" usemap="#gfg_map" />
+	<map name="gfg_map">
+		<!-- dividing rectangle into 4 equal parts -->
+		<area shape="rect"
+			coords="0,0, 250,250"
+			alt="GFG1"
+			
+			target="_blank" />
+		<area shape="rect"
+			coords="250,0, 500,250"
+			alt="GFG2"
+			
+			target="_blank" />
+		<area shape="rect"
+			coords="0,250, 250,500"
+			alt="GFG3"
+			
+			target="_blank" />
+		<area shape="rect"
+			coords="250,250, 500,500"
+			alt="GFG4"
+			
+			target="_blank" />
+	</map>
+</body>
 
-    <body>
-        <h1>Welcome to SMS</h1>
-    </body>
 </html>
