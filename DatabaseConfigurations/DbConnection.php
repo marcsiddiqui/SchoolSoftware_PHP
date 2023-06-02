25bd7cbaaa5e35dcd4d315023685245a9a5efe3f
Muhammad Arsalan <marcsiddiqui@gmail.com>
2023-06-02 09:49:50 +0500

    db change

--- DatabaseConfigurations/DbConnection.php
+++ DatabaseConfigurations/DbConnection.php
@@ -2,7 +2,8 @@
 
     function GetConnection()
     {
-        $con = mysqli_connect("localhost", "root", "", "schoolmanagementsystem") or die("Unable to Connect with Server: " . mysqli_connect_error());
+        $con = mysqli_connect("localhost", "root", "", "schoolmanagementsystem") 
+            or die("Unable to Connect with Server: " . mysqli_connect_error());
         return $con;
     }
 
